1a30e7abeadb7681d7345b8117de0253efdbb6cd
Adam Kariv <adam.kariv@gmail.com>
2019-10-06 14:13:32 +0300

    +travis

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,2 @@
-docker build . -t hasadna/datacity-backend:latest
+docker build . --cache-from hasadna/datacity-backend -t hasadna/datacity-backend:latest
 docker push hasadna/datacity-backend:latest
