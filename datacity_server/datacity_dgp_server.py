6aaa579e9b88a6ccae15e32159816e7f2dc66a26
Adam Kariv <adam.kariv@gmail.com>
2019-10-09 14:14:24 +0300

    Add schools taxonomies

--- datacity_server/datacity_dgp_server.py
+++ datacity_server/datacity_dgp_server.py
@@ -1,12 +1,14 @@
 import os
+import copy
 
 from dgp_server.blueprint import DgpServer
 
-from dataflows import Flow, add_computed_field, printer, duplicate, \
-    set_type, set_primary_key, dump_to_sql, add_field, join_with_self
+from dataflows import Flow, add_computed_field, duplicate, \
+    set_type, set_primary_key, dump_to_sql, add_field, join_with_self, \
+    PackageWrapper, load
 from dgp.core import Context, Config, BaseDataGenusProcessor
-from dgp.taxonomies import Taxonomy
-from dgp.config.consts import RESOURCE_NAME
+from dgp.config.consts import RESOURCE_NAME, CONFIG_TAXONOMY_CT, \
+    CONFIG_TAXONOMY_ID
 
 
 from .datacity_dgp import DataCityDGP
@@ -69,7 +71,7 @@ class ConfigStorerDGP(BaseDataGenusProcessor):
                 resources=TARGET
             ),
             add_field('config', 'object', saved_config, resources=TARGET),
-            add_field('fields', type='object', 
+            add_field('fields', type='object',
                       default=self.collate_values(fields), resources=TARGET),
             join_with_self(
                 TARGET,
@@ -98,6 +100,68 @@ class ConfigStorerDGP(BaseDataGenusProcessor):
         )
 
 
+class DBPreparerDGP(BaseDataGenusProcessor):
+
+    def __init__(self, config, context, lazy_engine):
+        super().__init__(config, context)
+        self.lazy_engine = lazy_engine
+
+    def add_missing_fields(self):
+        def func(package: PackageWrapper):
+            cts = {}
+            for ct in self.config.get(CONFIG_TAXONOMY_CT):
+                cts[ct['name'].replace(':', '-')] = ct
+
+            descriptor = copy.deepcopy(package.pkg.descriptor)
+            resource = None
+            for res in descriptor['resources']:
+                if res['name'] == RESOURCE_NAME:
+                    resource = res
+                    fields = res['schema']['fields']
+                    for field in fields:
+                        if field['name'] in cts:
+                            cts.pop(field['name'])
+                    print('MISSING:', list(cts.keys()))
+                    cts = list(cts.values())
+                    for ct in cts:
+                        ct_name = ct['name'].replace(':', '-')
+                        fields.append(dict(
+                            name=ct_name,
+                            type=ct['dataType'],
+                            **ct.get('options', {})
+                        ))
+                        if ct.get('unique'):
+                            res['schema']['primaryKey'].append(ct_name)
+                    fields.append(dict(
+                        name='_source', type='string'
+                    ))
+                    res['schema']['primaryKey'].append('_source')
+            if resource is not None:
+                descriptor['resources'] = [resource]
+                table_name = self.config.get(CONFIG_TAXONOMY_ID)\
+                    .replace('-', '_')
+                Flow(
+                    load((descriptor, [[]])),
+                    dump_to_sql(
+                        dict([
+                            (table_name, {
+                                'resource-name': RESOURCE_NAME,
+                                'mode': 'update'
+                            })
+                        ]),
+                        engine=self.lazy_engine(),
+                    )
+                ).process()
+            yield package.pkg
+            yield from package
+        return func
+
+    def flow(self):
+        return Flow(
+            self.add_missing_fields()
+        )
+
+
 class DatacityDgpServer(DgpServer):
 
     def __init__(self):
@@ -113,6 +177,10 @@ class DatacityDgpServer(DgpServer):
 
     def publish_flow(self, config: Config, context: Context):
         super_dgps = super().publish_flow(config, context)
+        super_dgps.insert(
+            0,
+            DBPreparerDGP(config, context, self.lazy_engine())
+        )
         super_dgps.append(
             ConfigStorerDGP(config, context, self.lazy_engine())
         )
--- datacity_server/processors/__init__.py
+++ datacity_server/processors/__init__.py
@@ -1,10 +1,11 @@
-import geocoder
-import requests
+import copy
 
-from dataflows import Flow, add_field, ResourceWrapper
-from dgp.core.base_enricher import BaseEnricher, \
-    DatapackageJoiner, ColumnTypeTester
-from dgp.config.consts import RESOURCE_NAME, CONFIG_PRIMARY_KEY
+from dataflows import Flow, ResourceWrapper, PackageWrapper, load, dump_to_sql
+from dgp.core.base_enricher import BaseEnricher, DatapackageJoiner
+from dgp.config.consts import RESOURCE_NAME, CONFIG_PRIMARY_KEY, \
+    CONFIG_TAXONOMY_CT, CONFIG_TAXONOMY_ID
+
+from .geo import AddressFixer, GeoCoder, GeoProjection  #noqa
 
 
 class StreamingDuplicateRemover(BaseEnricher):
@@ -40,7 +41,8 @@ class MunicipalityNameToCodeEnricher(DatapackageJoiner):
 
     REQUIRED_COLUMN_TYPES = ['municipality:name']
     PROHIBITED_COLUMN_TYPES = ['municipality:code']
-    REF_DATAPACKAGE = 'http://next.obudget.org/datapackages/lamas-municipal-data/datapackage.json'
+    REF_DATAPACKAGE = 'http://next.obudget.org/datapackages/' +\
+        'lamas-municipal-data/datapackage.json'
     REF_KEY_FIELDS = ['name_municipality']
     REF_FETCH_FIELDS = ['symbol_municipality_2015']
     SOURCE_KEY_FIELDS = ['municipality-name']
@@ -81,88 +83,3 @@ class FilterEmptyFields(BaseEnricher):
 
     def postflow(self):
         return Flow(self.work())
-
-
-class AddressFixer(BaseEnricher):
-
-    def test(self):
-        return True
-
-    def combine_addresses(self, row):
-        addresses = [
-            str(row[field])
-            for field in
-            ('address-' + suffix
-             for suffix in ('full', 'street', 'house-number', 'city'))
-            if field in row and row.get(field)
-        ]
-        if len(addresses) > 0:
-            row['address-full'] = ', '.join(addresses)
-        return row
-
-    def address_fixer(self):
-        def func(package):
-            address_fields = [
-                f['name'].replace('address-', '')
-                for f in package.pkg.descriptor['resources'][-1]['schema']['fields']
-                if f['name'].startswith('address-')
-            ]
-            if len(address_fields) > 0 and 'full' not in address_fields:
-                package.pkg.descriptor['resources'][-1]['schema']['fields'].append(dict(
-                    name='address-full',
-                    columnType='address:full',
-                    type='string'
-                ))
-            yield package.pkg
-            for i, res in enumerate(package):
-                if i == len(package.pkg.resources) - 1:
-                    yield (self.combine_addresses(r) for r in res)
-                else:
-                    yield res
-        return func
-
-    def postflow(self):
-        return Flow(
-            self.address_fixer(),
-        )
-
-
-class GeoCoder(ColumnTypeTester):
-
-    REQUIRED_COLUMN_TYPES = ['address:full']
-    PROHIBITED_COLUMN_TYPES = ['location:lat', 'location:lon']
-
-    def geocode(self):
-        session = requests.Session()
-        cache = {}
-
-        def func(row):
-            address = row.get('address-full')
-            if address and address.strip():
-                address = address.strip()
-                for prefix in ('שד', 'רח', 'רחוב'):
-                    if address.startswith(prefix + ' '):
-                        address = address[len(prefix)+1:]
-                        break
-                if address in cache:
-                    result = cache[address]
-                else:
-                    g = geocoder.osm(address, session=session, url='https://geocode.datacity.org.il/')
-                    if g.ok and g.lat and g.lng:
-                        result = (g.lat, g.lng)
-                    else:
-                        result = None
-                    cache[address] = result
-                if result:
-                    row['location-lat'], row['location-lon'] = result
-
-        return func
-
-    def conditional(self):
-        return Flow(
-            add_field('location-lat', 'number',
-                      resources=RESOURCE_NAME, columnType='location:lat'),
-            add_field('location-lon', 'number',
-                      resources=RESOURCE_NAME, columnType='location:lon'),
-            self.geocode()
-        )
